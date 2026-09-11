e096cec315f8d6b67edd9f5a24019a8fdc510cc5
Gleb1601 <gleb.zhukov.1601@gmail.com>
2026-09-11 15:09:06 +0300

    simplified and finished the dense class

--- classes.py
+++ classes.py
@@ -1,38 +1,26 @@
-from numpy.random import random
-from numpy import array, sqrt, outer
-
-# THE PARENT CLASS, FROM WHICH ALL OTHER LAYERS DERIVE #
-class Layer:
-    def __init__(self, previous_layer=None, name='parent_layer'):
-        self.name = name
-        self.previous_layer = previous_layer
-        if self.previous_layer != None:
-            self.previous_layer.next_layer = self 
-        self.next_layer = None
+import numpy as np
 
 # THE DENSE LAYER CLASS #
-class Dense(Layer):
-    def __init__(self, input_shape, output_shape, name='dense_layer', previous_layer=None):
-        super().__init__(previous_layer, name)
-        self.input_shape = input_shape
-        self.output_shape = output_shape
-        self.weights = random((self.input_shape, self.output_shape))
-        self.bias = random((1, self.output_shape))
+class Dense:
+    def __init__(self, weights_shape):
+        self.input_shape = weights_shape[0]
+        self.output_shape = weights_shape[1]
+        self.weights = np.random.random((self.input_shape, self.output_shape))
+        self.bias = np.random.random((1, self.output_shape))
 
     def forward(self, input):
         self.input = input
-        self.output = self.input.dot(self.weights)
-        if self.next_layer == None:
-            return self.output
-        self.next_layer.forward(self.output)
+        self.output = self.input.dot(self.weights) + self.bias
+        return self.output
 
     def backward(self, gradient, lr):
-        weight_grad = outer(self.input, gradient)
-        bias_grad = gradient 
+
+        input_grad = gradient.dot(self.weights.T)
+        weight_grad = self.input.T.dot(gradient)
+        bias_grad = np.sum(gradient, axis=0, keepdims=True) / len(y)
+
         self.weights -= lr * weight_grad
         self.bias -= lr * bias_grad
 
-        input_grad = gradient.dot(self.weights.T)
+        return input_grad
 
-        if self.previous_layer != None:
-            self.previous_layer.backward(input_grad)
